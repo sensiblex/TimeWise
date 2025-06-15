16481ec43db85decc8c8863661d3980d342499d3
Alexander <panfilov.alexander@bk.ru>
2025-06-15 04:25:26 +0300

    add test

---
+++ Tests/Test_activity.py
@@ -0,0 +1,11 @@
+import unittest
+from activities import *
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        activity = Activity('some active', 'sport', 2)
+        self.assertEqual(activity.name, 'some active', 'activity name has problem')
+        self.assertEqual(activity.type_of_activity, 'sport')
+
+if __name__ == '__main__':
+    unittest.main()
