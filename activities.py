5b36f1c7f0fa6c000dc57b117d5c9b93b3fb78e7
Alexander <panfilov.alexander@bk.ru>
2025-06-15 15:54:29 +0300

    add new functions to edit activity and storage

--- activities.py
+++ activities.py
@@ -1,14 +1,23 @@
 class Activities:
     def __init__(self):
-        self.activities = []
+        self.activities = {}
 
     def __str__(self):
-        for i in self.activities:
-            print(i)
+        for k,v in self.activities.items():
+            print(v)
         return ''
 
     def add(self, activity):
-        self.activities.append(activity)
+        self.activities[len(self.activities)+1] = activity
 
-    def remove(self, activity):
-        self.activities.remove(activity)
+    def remove(self, n):
+        del self.activities[n]
+        temp = self.activities
+        self.activities = {}
+        ln = 1
+        for k in temp.values():
+            self.activities[ln] = k
+            ln += 1
+
+    def rename(self, n, new_name):
+        self.activities[n].name = new_name
