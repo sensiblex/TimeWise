4bd083dbbdf3f4a43b36359b7a154488fb22b134
Alexander <panfilov.alexander@bk.ru>
2025-06-17 02:21:57 +0300

    solve the porblem with close bd

--- activities.py
+++ activities.py
@@ -3,50 +3,41 @@ import sqlite3 #TODO реализовать работу с бд
 class Activities:
     def __init__(self, bd='activities.db' ):
         self.db_name = bd
-        self.conn = None
         self._create_table()
-
-    # def _create_connection(self):
-    #     try:
-    #         self.conn = sqlite3.connect(self.db_name)
-    #         self.conn.row_factory = sqlite3.Row
-    #         print('Connected to database')
-    #     except sqlite3.Error as e:
-    #         self.conn = None
-    #         print(e)
 
     def _create_table(self):
         try:
-            with sqlite3.connect(self.db_name) as self.conn:
-                cursor = self.conn.cursor()
+            with sqlite3.connect(self.db_name) as conn:
+                cursor = conn.cursor()
                 cursor.execute("""CREATE TABLE IF NOT EXISTS activities (
                 time TEXT,
                 name TEXT,
                 type_of_activity TEXT,
                 duration TEXT
                 )""")
-                self.conn.commit()
+                conn.commit()
                 print('Table created successfully')
         except sqlite3.Error as e:
             print(e)
 
     def add(self, activity): #TODO Реализовать добавление активностей
         try:
-            with sqlite3.connect(self.db_name) as self.conn:
-                cursor = self.conn.cursor()
+            with sqlite3.connect(self.db_name) as conn:
+                cursor = conn.cursor()
                 __time = str(activity.time)
                 __name = activity.name
                 __type_of_activity = activity.type_of_activity
                 __duration = str(activity.duration)
                 cursor.execute("INSERT INTO activities (time, name, type_of_activity, duration) VALUES (?, ?, ?, ?)", (__time, __name, __type_of_activity, __duration))
-                self.conn.commit()
+                conn.commit()
         except sqlite3.Error as e:
             print(e)
+
     def show(self):
         try:
-            with sqlite3.connect(self.db_name) as self.conn:
-                self.conn.row_factory = sqlite3.Row
-                cursor = self.conn.cursor()
+            with sqlite3.connect(self.db_name) as conn:
+                conn.row_factory = sqlite3.Row
+                cursor = conn.cursor()
                 cursor.execute("SELECT rowid, * FROM activities")
                 rows = cursor.fetchall()
                 if rows:
@@ -55,14 +46,14 @@ class Activities:
                         print(el)
                 else:
                     print('Пустовато :(')
-
         except sqlite3.Error as e:
             print(e)
+
     def remove(self, n): #TODO Удаление активности
         try:
-            with sqlite3.connect(self.db_name) as self.conn:
-                cursor = self.conn.cursor()
+            with sqlite3.connect(self.db_name) as conn:
+                cursor = conn.cursor()
                 cursor.execute("DELETE FROM activities WHERE rowid = (?)", (n,))
-                self.conn.commit()
+                conn.commit()
         except sqlite3.Error as e:
             print(e)
