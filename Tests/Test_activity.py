e5bc6d94c15d05035911640a938fdb9249662fd4
Alexander <panfilov.alexander@bk.ru>
2025-06-21 01:32:59 +0300

    add first test function

--- Tests/Test_activity.py
+++ Tests/Test_activity.py
@@ -1,11 +1,17 @@
 import unittest
 from activity import *
+from unittest.mock import MagicMock
 
 class MyTestCase(unittest.TestCase): #TODO сделать норм тесты
-    def test_something(self):
-        activity = Activity('some active', 'sport', 2)
-        self.assertEqual(activity.name, 'some active', 'activity name has problem')
-        self.assertEqual(activity.type_of_activity, 'sport')
+    def test_init(self):
+        activity = Activity('some active', 'sport', 60)
+        self.assertEqual('some active', activity.name)
+        self.assertEqual('sport', activity.type_of_activity)
+        self.assertEqual(60, activity.duration)
+
+    def test_data(self):
+        ...
+
 
 if __name__ == '__main__':
     unittest.main()
